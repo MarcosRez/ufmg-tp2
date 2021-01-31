bb320d42960805c880027d30bc7fe5b60c3119ad
Arthurlkf <78284212+Arthurlkf@users.noreply.github.com>
2021-01-30 21:35:24 -0300

    Teste

--- matriz/matrixmain.cpp
+++ matriz/matrixmain.cpp
@@ -4,23 +4,23 @@
 int main() {
     Matrix Y(3,2);
     Matrix X(4,1);
-    
+
     std::cout << "Y:: " << std::endl;
     Y.print();
-    std::cout << "Numero de linhas de Y:: " << Y.getRows();
+    std::cout << "Nummmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmero de linhas de Y:: " << Y.getRows();
     std::cout << "Numero de colunas de Y:: " << Y.getCols();
-   
+
     std::cout << "Transposta de Y:: " << std::endl;
     Y.transpose();
     Y.print();
     std::cout << "Numero de linhas de Y:: " << Y.getRows();
     std::cout << "Numero de colunas de Y:: " << Y.getCols();
-    
+
     std::cout << std::endl << "X:: " << std::endl;
     X.print();
-    
+
     std::cout << "Numero de linhas de X:: " << X.getRows();
     std::cout << "Numero de colunas de X:: " << X.getCols();
-   
+
     return 0;
 }
