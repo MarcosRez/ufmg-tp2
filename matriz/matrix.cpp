104003a96ee7e1acf0108b92c31440f7a68a16f4
Vilela <72838984+Mateus-Vilela@users.noreply.github.com>
2021-01-30 21:59:56 -0300

    Update matrix.cpp

--- matriz/matrix.cpp
+++ matriz/matrix.cpp
@@ -1,12 +1,14 @@
 #include "matrix.h"
 using namespace std;
 
-// contrutor default - cria uma matriz sem nRows e nCols
+// contrutor default - cria uma matriz vazia com nRows = nCols = 0
 Matrix::Matrix(){
     nRows = 0;
     nCols = 0;
 }
 
+// contrutor parametrico 1 - cria uma matriz com nRows  = rows, nCols = cols e 
+// com todos os elementos iguais a elem (double)
 Matrix::Matrix(int rows, int cols){
 
     nRows = rows;
@@ -26,7 +28,6 @@ Matrix::Matrix(int rows, int cols){
 
 }
 
-
 // destrutor
 Matrix::~Matrix() {
 
@@ -36,14 +37,17 @@ Matrix::~Matrix() {
 
 }
 
+// obtem o numero de linhas
 int Matrix::getRows() const {
     return nRows;
 }
 
+// obtem o numero de colunas
 int Matrix::getCols() const {
     return nCols;
 }
 
+// retorna uma matriz transposta
 void Matrix::transpose() {
 
     Matrix *Aux = new Matrix(nRows, nCols);
@@ -66,6 +70,7 @@ void Matrix::transpose() {
 
 }
 
+// imprime o conteudo da matriz
 void Matrix::print() const {
 
     for(int i = 0; i < nRows; i++) {
